f8e1397ffe9162c01e1ca93b14b425967159419b
selmagassab <selma.gassab@esprit.tn>
2022-11-01 02:41:09 +0100

    update test unitaire junit+mockito

--- src/test/java/OperateurServiceImplTest.java
+++ src/test/java/OperateurServiceImplTest.java
@@ -4,75 +4,97 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import tn.esprit.rh.achat.AchatApplication;
 import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.IOperateurService;
 
-@RunWith(SpringRunner.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(classes = AchatApplication.class)
-
+@RunWith(SpringRunner.class)
 
 public class OperateurServiceImplTest {
 
-	@MockBean
-	private OperateurRepository operateurRepository;
-	private Operateur operateur1 = new Operateur(1L,"test1","tes1","546125",null);
-	private Operateur operateur2 = new Operateur(2L,"test2","test2","546125",null);
-
-	@Autowired
-	IOperateurService operateurService;
-	
-	  @Test
-		public void addOperateurTest() {
-	    	when(operateurRepository.save(operateur1)).thenReturn(operateur1);
-	    	assertNotNull(operateur1);
-	    	
-	    	Operateur persisted = operateurService.addOperateur(operateur1);
-			assertEquals(operateur1, persisted); 
-	    	
-			System.out.println("add works !");
-		}
-	
-	 @Test 
-	    public void retrieveAllOperateursTest() {
-	    	when(operateurRepository.findAll()).thenReturn(Stream
-	    			.of(operateur1,operateur2)
-	    			.collect(Collectors.toList()));
-	    	
-	    	assertEquals(2,operateurService.retrieveAllOperateurs().size());
-	    	System.out.println("Retrieve all operators works !");
-	    }
-	
-	   @Test 
-	    public void UpdateOperateurTest() {
-	    	when(operateurRepository.save(operateur1)).thenReturn(operateur1);
-	    	assertNotNull(operateur1);
-	    	assertEquals(operateur1, operateurService.updateOperateur(operateur1));
-	    	System.out.println("Update works!");
-	    }
-	    
+	 @Autowired
+	    IOperateurService os;
 	    @Test
-	    public void retrieveOperateurTest() {
-	    	when(operateurRepository.findById(operateur1.getIdOperateur())).thenReturn(Optional.of(operateur1));
-	    	assertEquals(operateur1, operateurService.retrieveOperateur(operateur1.getIdOperateur()));
-	    	System.out.println("Retrieve operator by id works !");
+	    @Order(2)
+	    public void testretrieveAllOperateurs(){
+	       int listOperateur = os.retrieveAllOperateurs().size();
+	        List<Operateur> listOperateurs = os.retrieveAllOperateurs();
+	        Assertions.assertEquals(listOperateur,listOperateurs.size());
 	    }
-	
+
+	    @Test
+	    @Order(3)
+	     public void testretrieveOperateur(){
+	       Operateur op = os.addOperateur(Operateur.builder()
+	               .nom("Soua")
+	               .prenom("Amira")
+	               .password("root")
+	               .build());
+	        Assertions.assertEquals(op.getIdOperateur() , os.retrieveOperateur(op.getIdOperateur()).getIdOperateur()) ;
+			System.out.println("retrieve junit !");
+
+	    }
+
+	    @Test
+	    @Order(1)
+	    public void testaddOperateur(){
+	        Operateur op = os.addOperateur(Operateur.builder()
+	                .nom("Gassab")
+	                .prenom("Selma")
+	                .password("root")
+	                .build());
+	        Assertions.assertNotNull(op);
+			System.out.println("add junit !");
+
+
+
+	    }
+
+	    @Test
+	    @Order(5)
+	    public void testdeleteOperateur(){
+	       Operateur op = os.addOperateur(Operateur.builder()
+	               .nom("Maria")
+	               .prenom("Trifi")
+	               .password("root")
+	               .build());
+	        os.deleteOperateur(op.getIdOperateur());
+	        //Assertions.assertEquals(- 1,os.retrieveAllOperateurs().size());
+	        Assertions.assertNull(os.retrieveOperateur(op.getIdOperateur()));
+			System.out.println("delete junit !");
+
+
+	    }
+
+	    @Test
+	    @Order(4)
+	    public void tesupdateOperateur(){
+	       Operateur op = os.addOperateur(Operateur.builder()
+	               .nom("Farhat")
+	               .prenom("Sabaa")
+	               .password("root")
+	               .build());
+	        op.setPrenom("test");
+	        os.updateOperateur(op);
+	        Assertions.assertEquals("test", os.updateOperateur(op).getPrenom());
+			System.out.println("update junit !");
+
+
+	    }
+
 
 }
