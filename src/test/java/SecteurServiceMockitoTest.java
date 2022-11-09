d48e888ad91a98c3db1eec07cd57843d2b9f3097
MohsniNour <nourelhouda.mohsni@esprit.tn>
2022-11-09 23:49:38 +0100

    fixed name test

--- src/test/java/SecteurServiceMockitoTest.java
+++ src/test/java/SecteurServiceMockitoTest.java
@@ -3,7 +3,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -13,16 +16,21 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
 import tn.esprit.rh.achat.AchatApplication;
+import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.entities.SecteurActivite;
+import tn.esprit.rh.achat.repositories.ProduitRepository;
 import tn.esprit.rh.achat.repositories.SecteurActiviteRepository;
+import tn.esprit.rh.achat.services.ProduitServiceImpl;
 import tn.esprit.rh.achat.services.SecteurActiviteServiceImpl;
 
 
 @SpringBootTest(classes = AchatApplication.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
-@RunWith(MockitoJUnitRunner.class)
-public class SecteurServiceMockito {
+public class SecteurServiceMockitoTest {
+
 
 	@Mock
 	 SecteurActiviteRepository saRepositoryMock;
@@ -93,4 +101,6 @@ public class SecteurServiceMockito {
 
 
 	 }
+	 
+
 }
